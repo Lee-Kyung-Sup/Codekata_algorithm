8cf088332d1efffc88379a86dc02caeaa0698140
Lee-Kyung-Sup(이경섭) <120997897+Lee-Kyung-Sup@users.noreply.github.com>
2024-01-17 09:33:59 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public bool solution(int x) 
+    {
+        bool answer = true;
+        int sum = 0;
+        int temp = x;
+        while(temp>0)
+        {
+            sum += temp%10;
+            temp /= 10;
+        }
+        if(x%sum ==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
